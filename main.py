d31274ab01454551e1e0af61721a069ab03d2f4e
coemelo <merlosamuel@protonmail.ch>
2023-07-29 12:33:14 -0300

    User can see the right and almost right words

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 import random
 
-set = [("apple"), ("arrow"), ("beach"), ("bread"), ("bread"), ("cloud"), ("dance"), ("earth"), ("fight"), ("ghost"), ("happy"), ("knife"), ("jumbo"), ("mango"), ("laugh") ]
+set = [("apple"), ("arrow"), ("beach"), ("bread"), ("steam"), ("cloud"), ("dance"), ("earth"), ("fight"), ("ghost"), ("happy"), ("knife"), ("jumbo"), ("mango"), ("laugh") ]
 word = random.choice(set)
 
-def start(attemps = 5):
+def start():
 
     userWord = ''
+    attempts = 5
 
-    userAttempt = input(": ")
+    while attempts > 0:
+        userAttempt = input(": ")
 
-    if userAttempt == word:
-        print("")
-        return True
-    
-    for i in range(len(word)):
-        if userAttempt[i] == word[i]:
-            userWord += userAttempt[i].upper()
-        else:
-            userWord += userAttempt[i]
+        if userAttempt == word:
+            print(userAttempt.upper() + "\nCongratulations!\n")
+            return True
+        
+        for i in range(len(word)):
+            if userAttempt[i] == word[i]:
+                userWord += userAttempt[i].upper()
+            elif userAttempt[i] in word:
+                userWord += userAttempt[i].lower()
+            else:
+                userWord += '_'
 
-    
+        attempts -= 1
 
-    print(userWord + "\n")
+        print("\n" + userWord + "\n")
+
+    return False
 
 print("\nVamos Começar!\n")
 
