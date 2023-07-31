943b7602302dc8d10668234d732170ec3255b0e3
coemelo <merlosamuel@protonmail.ch>
2023-07-31 09:41:49 -0300

    Feat ErrorHandler: the user only can type a 5 letters (or more) word, receiving an error message if not without crashing the app

--- controller.py
+++ controller.py
@@ -10,24 +10,29 @@ def start():
     while attempts > 0:
 
         userWord = [''] * 5
-        userAttempt = input(": ")
 
-        if userAttempt == word:
-            print("\n" + userAttempt.upper() + "\nCongratulations!\n")
-            return True
-        
-        for i in range(len(word)):
+        try:
+            userAttempt = input(": ")
+
+            if userAttempt == word:
+                print("\n" + userAttempt.upper() + "\nCongratulations!\n")
+                return True
             
-            if userAttempt[i] == word[i]:
-                userWord[i] = userAttempt[i].upper()
-            elif userAttempt[i] in word:
-                userWord[i] = userAttempt[i].lower()
-            else:
-                userWord[i] = '_'
+            for i in range(len(word)):
+                
+                if userAttempt[i] == word[i]:
+                    userWord[i] = userAttempt[i].upper()
+                elif userAttempt[i] in word:
+                    userWord[i] = userAttempt[i].lower()
+                else:
+                    userWord[i] = '_'
 
-        attempts -= 1
+            attempts -= 1
 
-        print("\n" + "".join(userWord) + "\n")
+            print("\n" + "".join(userWord) + "\n")
+
+        except IndexError:
+            print("\nSorry, it has to be a five (5) letters word\n")
 
     print("\nGAME OVER! The Word was: " + word + "\n")
 
